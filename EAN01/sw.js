a9a1bf2fe2a37f8b1785c743dac5a5b0a783221c
angelmicelti <angelmicelti@users.noreply.github.com>
2025-12-12 17:25:19 +0100

    Create sw.js

---
+++ EAN01/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'ean01-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalación y almacenamiento en caché de archivos base
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Estrategia: Buscar en caché, si no está, ir a la red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
